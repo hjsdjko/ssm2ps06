0037d8b7ae57a5b95b8867aa5af2ddbdc15b5029
jsxs <2261203961@qq.com>
2024-05-10 20:18:07 +0800

    first commit

---
+++ uploadGithub.bat
@@ -0,0 +1,12 @@
+@echo off
+
+
+for /D %%i in (*) do (
+    if not "%%i"=="mp-weixin" (
+        
+         git init && git add . && git commit -m "first commit" && git branch -M main && git remote add origin https://github.com/hjsdjko/%%i.git && git push -u origin main
+        
+    )
+)
+
+pause
